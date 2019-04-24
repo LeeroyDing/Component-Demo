c5bf0ea53f91d63c646749962d66cd25c5cb8bd4
Leeroy Ding <sicheng.ding@ig.com>
2019-04-24 13:09:44 +0100

    Initial Commit

---
+++ Component Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Component Demo
+//
+//  Created by Sicheng Ding on 24/04/2019.
+//  Copyright © 2019 IG Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
